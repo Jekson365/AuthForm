d1230f20e458111fecb1b69206f8938185765367
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-04-26 21:17:25 +0400

    cc

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,76 @@
 import { useState } from 'react'
+import './styles/index.scss'
 import { useForm } from 'react-hook-form'
 
-function App() {
-  const { register, handleSubmit, formState: { errors } } = useForm()
+type NewUser = {
+  username: string,
+  email: string,
+  password: string,
+  rPassword: string
+}
 
-  console.log(errors)
+
+function App() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm()
+
+  const onSubmit = (data: NewUser) => {
+    console.log(data)
+  }
+
+  if (errors) {
+    console.log(errors.name?.message)
+  }
+
+
   return (
     <>
-    <form>
-      
-    </form>
+      <form
+        onSubmit={handleSubmit((data) => onSubmit({ username: data.name, email: data.email, password: data.passowrd, repeatPassword: data.rPassword }))}
+      >
+        <h1>Registration</h1>
+        <div className="input-group pos-rel">
+          <div className="errorM">{errors.username?.message}</div>
+          <label htmlFor="name">Username</label>
+          <input
+            {...register("username", {
+              required: "username is required",
+              minLength: 3,
+              maxLength: 12,
+            })}
+            type="text" name='username' placeholder='name' className='inputfield' />
+        </div>
+        <div className="input-group pos-rel">
+            <div className="errorM">{errors.email?.message}</div>
+          <label htmlFor="email">Email</label>
+          <input
+            {...register("email", {
+              required: "email is required",
+              pattern: /^\S+@\S+$/i,
+            })}
+            type="email" name='email' placeholder='email' className='inputfield' />
+        </div>
+        <div className="input-group pos-rel">
+            <div className="errorM">{errors.password?.message}</div>
+          <label htmlFor="name">password</label>
+          <input
+            {...register("password", {
+              required: "password is required",
+              minLength: 3,
+              maxLength: 12
+            })}
+            type="password" name='password' placeholder='password' className='inputfield' />
+        </div>
+        <div className="input-group pos-rel">
+            <div className="errorM">{errors.repeatPassword?.message}</div>
+          <label htmlFor="rpassword">Repeat password</label>
+          <input
+            {...register("repeatPassword", {
+              required: "password is required",
+            })}
+            type="password" name='repeatPassword' placeholder='repeat password' className='inputfield' />
+        </div>
+        <input type="submit" className='submit' />
+      </form>
     </>
   )
 }
